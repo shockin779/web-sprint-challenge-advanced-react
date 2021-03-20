901a5ac79102851ebe5b6f6cb0e03050f8009646
Sean Hockin <sean.hockin@gmail.com>
2021-03-20 13:23:33 -0600

    React tests completed; MVP completed

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,39 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React, { useReducer } from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+    const formHeaderEl = screen.getByText('Checkout Form');
 
-test("form shows success message on submit with form details", () => {});
+    expect(formHeaderEl).toBeDefined();
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+
+    const fNameInput = screen.getByLabelText(/First Name/i);
+    const lNameInput = screen.getByLabelText(/Last Name/i);
+    const addressInput = screen.getByLabelText(/Address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+    const submitButtonEl = screen.getByRole('button', {name: 'Checkout'});
+
+    userEvent.type(fNameInput, 'Sean');
+    userEvent.type(lNameInput, 'Hockin')
+    userEvent.type(addressInput, '864 E 350 N')
+    userEvent.type(cityInput, 'Layton')
+    userEvent.type(stateInput, 'UT')
+    userEvent.type(zipInput, '84041')
+
+    userEvent.click(submitButtonEl)
+
+    const successDivEl = screen.getByTestId('successMessage');
+
+    expect(successDivEl).toBeDefined();
+
+});
